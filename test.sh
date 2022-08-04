b45df6df613917698307fc102b3a49438fc77d76
Evelyn92 <Evelynjia92@gmail.com>
2022-08-04 11:29:35 +0200

    recommit

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/students/yiwei/yiwei_gitlab/MISSFormer-bridge/MISSFormer/
+chmod +x test.sh
+/home/students/yiwei/anaconda3/envs/missformer/bin/python /home/students/yiwei/yiwei_gitlab/MISSFormer-bridge/MISSFormer/test.py "$@"
+
+
+
+
